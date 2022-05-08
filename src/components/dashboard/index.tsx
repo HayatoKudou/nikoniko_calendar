034bac4e3986e047294bb7a236a505499b1cb040
工藤颯斗 <kudo@tetemarche.co.jp>
2022-05-08 18:37:04 +0900

    feat: add TabPanel

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,5 +1,100 @@
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
 const Dashboard = () => {
-  return <div></div>;
+  const [value, setValue] = React.useState(0);
+  const [formOpen, setFormOpen] = React.useState(false);
+  const [formValue, setFormValue] = React.useState("");
+  const [tabList, setTabList] = React.useState([{ label: "ALL" }]);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    if (newValue !== undefined) {
+      setValue(newValue);
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    tabList.push({ label: formValue });
+    setTabList(tabList);
+    setFormOpen(false);
+    setFormValue("");
+  };
+
+  return (
+    <>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          {tabList.map((tab, index) => (
+            <Tab label={tab.label} {...a11yProps(index)} key={index} />
+          ))}
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <IconButton onClick={() => setFormOpen(!formOpen)}>
+              {formOpen ? <RemoveCircleIcon /> : <AddCircleIcon />}
+            </IconButton>
+            {formOpen && (
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  value={formValue}
+                  onChange={(e) => setFormValue(e.target.value)}
+                  size="small"
+                />
+              </form>
+            )}
+          </Box>
+        </Tabs>
+      </Box>
+      {tabList.map((tab, index) => (
+        <TabPanel value={value} index={index} key={index}>
+          {"hogehoge"}
+        </TabPanel>
+      ))}
+    </>
+  );
 };
 
 export default Dashboard;
