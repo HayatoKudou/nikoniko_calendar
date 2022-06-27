94c3cc949a7258cbc12e377980571ffb5e617af7
工藤颯斗 <kudo@tetemarche.co.jp>
2022-06-27 23:45:48 +0900

    wip

--- src/components/books/index.tsx
+++ src/components/books/index.tsx
@@ -1,16 +1,3 @@
-import CircleIcon from "@mui/icons-material/Circle";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import ModeEditIcon from "@mui/icons-material/ModeEdit";
-import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Typography from "@mui/material/Typography";
 import { useSnackbar } from "notistack";
 import * as React from "react";
 import { useRecoilValue } from "recoil";
@@ -18,10 +5,44 @@ import Config from "../../../config";
 import DeleteBook from "../../api/book/delete";
 import useBooks from "../../api/book/list";
 import { useMe } from "../../store/me";
-import { bookStatusColor, bookStatusName } from "../../util/book";
 import Update from "../books/update";
 import ConfirmDialog from "../confirm_dialog";
 import Spinner from "../spinner";
+import CustomTable from "./table";
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: string;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: "status",
+    numeric: false,
+    disablePadding: true,
+    label: "ステータス",
+  },
+  {
+    id: "category",
+    numeric: true,
+    disablePadding: false,
+    label: "カテゴリ",
+  },
+  {
+    id: "title",
+    numeric: true,
+    disablePadding: false,
+    label: "タイトル",
+  },
+  {
+    id: "description",
+    numeric: true,
+    disablePadding: false,
+    label: "本の説明",
+  },
+];
 
 const Books = () => {
   const me = useRecoilValue(useMe);
@@ -79,9 +100,6 @@ const Books = () => {
 
   return (
     <>
-      <Box sx={{ display: "flex", alignItems: "center", height: "80px" }}>
-        <Typography variant="h4">書籍管理</Typography>
-      </Box>
       {selectBook && (
         <Update
           book={selectBook}
@@ -90,52 +108,8 @@ const Books = () => {
           onSuccess={() => mutate(`${Config.apiOrigin}/api/${me.clientId}/user/list`)}
         />
       )}
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell size={"small"} />
-              <TableCell>ステータス</TableCell>
-              <TableCell align="right">カテゴリ</TableCell>
-              <TableCell align="center" sx={{ width: "30%" }}>
-                タイトル
-              </TableCell>
-              <TableCell align="center" sx={{ width: "40%" }}>
-                本の説明
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {response.books?.map((book: Book, index: number) => (
-              <TableRow key={index} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                <TableCell>
-                  <IconButton onClick={() => handleClickDeleteButton(book)}>
-                    <DeleteForeverIcon />
-                  </IconButton>
-                  <ConfirmDialog
-                    message={"本当に削除しますか？"}
-                    open={openDeleteConfirm}
-                    onClose={handleConfirmClose}
-                    handleSubmit={handleDeleteBook}
-                  />
-                  <IconButton onClick={() => handleEditBook(book)}>
-                    <ModeEditIcon />
-                  </IconButton>
-                </TableCell>
-                <TableCell>
-                  <Box sx={{ display: "flex", alignItems: "center" }}>
-                    <CircleIcon color={bookStatusColor(book.status)} fontSize={"small"} />
-                    {bookStatusName(book.status)}
-                  </Box>
-                </TableCell>
-                <TableCell align="left">{book.category}</TableCell>
-                <TableCell align="left">{book.title}</TableCell>
-                <TableCell align="left">{book.description}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <ConfirmDialog message={"本当に削除しますか？"} open={openDeleteConfirm} onClose={handleConfirmClose} handleSubmit={handleDeleteBook} />
+      <CustomTable headCells={headCells} books={response.books} handleEdit={handleEditBook} handleDelete={handleClickDeleteButton} />
     </>
   );
 };
---
+++ src/components/books/table.tsx
@@ -0,0 +1,291 @@
+import CircleIcon from "@mui/icons-material/Circle";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import Paper from "@mui/material/Paper";
+import { alpha } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import { visuallyHidden } from "@mui/utils";
+import * as React from "react";
+import { bookStatusColor, bookStatusName } from "../../util/book";
+
+type Order = "asc" | "desc";
+interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (event: React.MouseEvent<unknown>, property: any) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+  headCells: any;
+}
+interface EnhancedTableToolbarProps {
+  numSelected: number;
+  handleDelete: any;
+}
+interface Props {
+  headCells: any;
+  books: any;
+  handleEdit: any;
+  handleDelete: any;
+}
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
+  return order === "desc" ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const createSortHandler = (property: any) => (event: React.MouseEvent<unknown>) => {
+    onRequestSort(event, property);
+  };
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              "aria-label": "select all desserts",
+            }}
+          />
+        </TableCell>
+        <TableCell />
+        {props.headCells.map((headCell: any) => (
+          <TableCell
+            key={headCell.id}
+            align={"center"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
+  const { numSelected, handleDelete } = props;
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) => alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography sx={{ flex: "1 1 100%" }} color="inherit" variant="subtitle1" component="div">
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography sx={{ flex: "1 1 100%" }} variant="h6" id="tableTitle" component="div">
+          Nutrition
+        </Typography>
+      )}
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton onClick={handleDelete}>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
+
+export default function EnhancedTable(props: Props) {
+  const [order, setOrder] = React.useState<Order>("asc");
+  const [orderBy, setOrderBy] = React.useState("status");
+  const [selected, setSelected] = React.useState<readonly string[]>([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event: React.MouseEvent<unknown>, property: any) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelecteds = props.books.map((n: any) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected: readonly string[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const isSelected = (name: string) => selected.indexOf(name) !== -1;
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - props.books.length) : 0;
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <EnhancedTableToolbar numSelected={selected.length} handleDelete={props.handleDelete} />
+        <TableContainer>
+          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle" size="small">
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={props.books.length}
+              headCells={props.headCells}
+            />
+            <TableBody>
+              {stableSort(props.books, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row: any, index) => {
+                  const isItemSelected = isSelected(props.books.name);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, props.books.name)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.name}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            "aria-labelledby": labelId,
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <IconButton onClick={() => props.handleEdit(row)}>
+                          <ModeEditIcon />
+                        </IconButton>
+                      </TableCell>
+                      <TableCell align="center">
+                        <Box sx={{ display: "flex", alignItems: "center" }}>
+                          <CircleIcon color={bookStatusColor(row.status)} fontSize={"small"} />
+                          {bookStatusName(row.status)}
+                        </Box>
+                      </TableCell>
+                      <TableCell align="center">{row.category}</TableCell>
+                      <TableCell align="left" sx={{ width: "30%" }}>
+                        {row.title}
+                      </TableCell>
+                      <TableCell align="left" sx={{ width: "40%" }}>
+                        {row.description}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 33 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={props.books.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+}
