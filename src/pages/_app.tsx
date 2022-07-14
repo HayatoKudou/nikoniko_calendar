8ed4db871b81e6282fc7a5a9fe2dcb84eb2b0dd2
工藤颯斗 <kudo@tetemarche.co.jp>
2022-07-15 00:17:08 +0900

    wio

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import "../styles/globals.css";
 import { SnackbarProvider } from "notistack";
 import { RecoilRoot } from "recoil";
 import type { AppProps } from "next/app";
+import {StyledEngineProvider} from "@mui/material";
 
 function App({ Component, pageProps }: AppProps) {
   return (
+    <StyledEngineProvider injectFirst>
     <RecoilRoot>
       <SnackbarProvider
         maxSnack={3}
@@ -16,6 +18,7 @@ function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </SnackbarProvider>
     </RecoilRoot>
+    </StyledEngineProvider>
   );
 }
 
